861f2eec975f458d27c1bc1f4d350bfdb7d9d5cd
damagr <damagr@users.noreply.github.com>
2021-08-21 08:24:23 +0000

    Prettified Code!

--- index.js
+++ index.js
@@ -1,31 +1,32 @@
-import { Client, Intents } from 'discord.js';
-import axios from 'axios';
+import { Client, Intents } from "discord.js";
+import axios from "axios";
 
-const url = 'https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd'
+const url =
+  "https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd";
 let lastPrice;
 
 const client = new Client({
   intents: [Intents.FLAGS.GUILDS],
 });
 
-const BOT_ID = process.env['BOT_ID']
-const BOT_TOKEN = process.env['BOT_TOKEN']
+const BOT_ID = process.env["BOT_ID"];
+const BOT_TOKEN = process.env["BOT_TOKEN"];
 
-client.once('ready', () => {
-  console.log('We on . . .');
+client.once("ready", () => {
+  console.log("We on . . .");
 
-  const SERVER_ID = process.env['SERVER_ID']
+  const SERVER_ID = process.env["SERVER_ID"];
   const guild = client.guilds.cache.get(SERVER_ID);
   const BOT = guild.members.cache.get(BOT_ID);
 
   setInterval(async () => {
     getPrice();
-    BOT.user.setActivity('Ethereum: ' + lastPrice);
+    BOT.user.setActivity("Ethereum: " + lastPrice);
   }, 5000);
 });
 
-function getPrice(){
-  axios.get(url).then(response => {
+function getPrice() {
+  axios.get(url).then((response) => {
     lastPrice = response.data.ethereum.usd;
   });
 }
